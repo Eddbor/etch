b47e9951225364ac4c2663c7f3c1c39b09dbb7e1
EddBor <3ddbor@gmail.com>
2024-04-09 15:06:21 +0100

    Do 2 of the buttons
    Did 2 of the buttons. They work properly with eachother.
    The dark one can already be clicked doesnt work tho.
    Needs to work togheter with the other 2 too.

--- script.js
+++ script.js
@@ -5,83 +5,149 @@ let color1
 let color2
 let color3
 const refuse = document.querySelector('.refuse')
+const topbutton = document.querySelector('.buttons')
+
+function question(){
+    let size = +prompt("What's the size of the grid? (1-100)")
+    gridsize()
+    function gridsize(){
+        if(Number.isNaN(size)){
+            alert('A number please...')
+        }else if(size == ''){   
+            question()
+        }else if(size > 100){   
+            size = 100
+            grid(size , size)
+        }else if(size < 0){
+            size = 1
+            grid(size , size)
+        }else{
+            grid(size , size)
+        }
+    }
+
+    const btnrgb = document.querySelector('#btnrgb')
+    btnrgb.addEventListener('click' , () => {
+        for(let i = 0; i < size; i++){
+            const del = document.querySelector('.griddel')
+            del.remove()
+        }
+        btnrgb.classList.toggle('rgbon');
+        gridsize()
+    });
+
+    const btnreset = document.querySelector('#btnreset')
+    btnreset.addEventListener('click' , () => {
+        for(let i = 0; i < size; i++){
+            const del = document.querySelector('.griddel')
+            del.remove()
+        }
+        btnreset.classList.toggle('reseton');
+        gridsize()
+    });
+
+    const btndark = document.querySelector('#btndark')
+    btndark.addEventListener('click' , () => {
+        for(let i = 0; i < size; i++){
+            const del = document.querySelector('.griddel')
+            del.remove()
+        }
+        btndark.classList.toggle('darkon');
+        gridsize()
+    });
+}
+
+
 
 function grid(linenumb , columnumb){
     for(let i = 0; i < linenumb; i++){
         const line = document.createElement('div')
-        line.classList.add('line')
+        line.setAttribute('id' , 'line')
+        line.classList.add('griddel')
         container.appendChild(line)
         for(let i = 0; i < columnumb; i++){
             const square = document.createElement('div')
             square.classList.add('square')
             line.appendChild(square)
-            //Purples/Pinks/Reds
-            //square.addEventListener('mouseover', () => {
-            //    color1 = Math.floor(Math.random() * 100 + 120)
-            //    color2 = Math.floor(Math.random() * 255)
-            //    square.style.cssText = `background-color: rgb(${color1}, 5, ${color2}) ; border:solid 1px rgb(200, 5, 200)`
-            //})
+            squaregrid()
+            
+            
 
-            //only the bright RGBs
-            square.addEventListener('mouseover', () => {
-                test()
-                square.style.cssText = `background-color: rgb(${color1} , ${color2} , ${color3}) ; border:solid 1px rgb(200, 5, 200)`
-                refuse.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
-            })
+            
+            function squaregrid(){
+                if(!(btnrgb.classList.contains('rgbon'))){
+                    square.addEventListener('mouseover', () => {
+                        purplecol()
+                        square.style.cssText = `background-color: rgb(${color1} , ${color2} , ${color3}) ; border:solid 1px rgb(200, 5, 200)`
+                        refuse.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
+                        topbutton.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
+                    })
+                }else if((btnrgb.classList.contains('rgbon'))){
+                
+                square.addEventListener('mouseover', () => {
+                    brightcol()
+                    square.style.cssText = `background-color: rgb(${color1} , ${color2} , ${color3}) ; border:solid 1px rgb(200, 5, 200)`
+                    refuse.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
+                    topbutton.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
+                })
+            }
+            }
             //Reset to default
-            //square.addEventListener('mouseout', () => {
-            //    square.style.cssText = ''
-            //})
+            if(btnreset.classList.contains('reseton')){
+                square.addEventListener('mouseout', () => {
+                    square.style.cssText = ''
+                })
+            }
+
+            if(btndark.classList.contains('darkon') && !(btnrgb.classList.contains('rgbon'))){
+                purplecol()
+                square.style.cssText = `background-color: rgb(${color1} , ${color2} , ${color3}) , ; border:solid 1px rgb(200, 5, 200)`
+                refuse.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
+                topbutton.style.cssText = `-webkit-text-fill-color: rgb(${color1} , ${color2} , ${color3}) ; -webkit-text-stroke-width: 1px ; -webkit-text-stroke-color: rgb(${color1} , ${color2} , ${color3})`
+
+            }
         }
     }
 }
 
-function question(){
-    let size = +prompt("What's the size of the grid? (1-100)")
-    let numb
-    if(Number.isNaN(size)){
-        alert('A number please...')
-    }else if(size > 100){
-        numb = 100
-        grid(numb , numb)
-    }else if(size < 0){
-        numb = 1
-        grid(numb , numb)
-    }else{
-        grid(size , size)
-    }
+//function only purples
+function purplecol(){
+    color1 = Math.floor(Math.random() * 100 + 120)
+    color2 = 5
+    color3 = Math.floor(Math.random() * 255)
 }
 
-function test(){
-if(Math.floor(Math.random() * 10) < 4){
-    color1 = 255
-    if(Math.floor(Math.random() * 10) < 6){
-        color2 = Math.floor(Math.random() * 255)
-        color3 = 0
-    }else{
-        color3 = Math.floor(Math.random() * 255)
-        color2 = 0
-    }
-}else if(Math.floor(Math.random() * 10) < 7){
-    color2 = 255
-    if(Math.floor(Math.random() * 10) < 6){
+//function of only bright collors
+function brightcol(){
+    if(Math.floor(Math.random() * 10) < 4){
+        color1 = 255
+        if(Math.floor(Math.random() * 10) < 6){
+            color2 = Math.floor(Math.random() * 255)
+            color3 = 0
+        }else{
+            color3 = Math.floor(Math.random() * 255)
+            color2 = 0
+        }
+    }else if(Math.floor(Math.random() * 10) < 7){
+        color2 = 255
+        if(Math.floor(Math.random() * 10) < 6){
         color1 = Math.floor(Math.random() * 255)
-        color3 = 0
-    }else{
-        color3 = Math.floor(Math.random() * 255)
-        color1 = 0
+            color3 = 0
+        }else{
+            color3 = Math.floor(Math.random() * 255)
+            color1 = 0
+        }
+    }else if(Math.floor(Math.random() * 10) < 10){
+        color3 = 255
+        if(Math.floor(Math.random() * 10) < 6){
+            color2 = Math.floor(Math.random() * 255)
+            color1 = 0
+        }else{
+            color1 = Math.floor(Math.random() * 255)
+            color2 = 0
+        }
     }
-}else if(Math.floor(Math.random() * 10) < 10){
-    color3 = 255
-    if(Math.floor(Math.random() * 10) < 6){
-        color2 = Math.floor(Math.random() * 255)
-        color1 = 0
-    }else{
-        color1 = Math.floor(Math.random() * 255)
-        color2 = 0
-    }
-}
-console.log(`${color2} , ${color1} , ${color3}`)
+//console.log(`${color2} , ${color1} , ${color3}`)
 }
 
 question()
